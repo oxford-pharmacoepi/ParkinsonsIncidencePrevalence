be214e9866537c9d428ef8c3bc45a94e1c9c559c
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-01-13 17:17:22 +0000

    Update InstantiateStudyCohorts.R

--- 1_InstantiateCohorts/InstantiateStudyCohorts.R
+++ 1_InstantiateCohorts/InstantiateStudyCohorts.R
@@ -1,18 +1,50 @@
-# instantiate outcome cohorts
-info(logger, "- getting outcome definitions")
+library(CDMConnector)
+library(here)
 
-outcome_cohorts <- CDMConnector::readCohortSet(here(
+################################################################################
+#                                                                              #
+#                              Setting up for Drugs                            #
+#                                                                              #
+################################################################################
+
+outcome_cohorts_drugs <- CDMConnector::readCohortSet(here(
   "1_InstantiateCohorts",
-  "OutcomeCohorts"
+  "OutcomeCohortsDrugs"
 ))
 
-info(logger, "- getting outcomes")
+databaseName <- "..."
+server     <- Sys.getenv("DB_SERVER_name_database")
+server_dbi <- Sys.getenv("DB_SERVER_DBI_name_database")
+user       <- Sys.getenv("DB_USER")
+password   <- Sys.getenv("DB_PASSWORD")
+port       <- Sys.getenv("DB_PORT")
+host       <- Sys.getenv("DB_HOST")
+vocabularyDatabaseSchema <- "..."
+dbmsName <- "..."
+con <- DBI::dbConnect(RPostgres::Postgres(), dbname = server_dbi, port = port, host = host, user = user,
+                      password = password)
+cdm <- CDMConnector::cdm_from_con(con = con,
+                                  cdm_schema = '...',
+                                  write_schema = '...')
+                                  
+cdm <- CDMConnector::generateCohortSet(cdm = cdm,
+                                       cohortSet = outcome_cohorts_drugs,
+                                       cohortTableName = '...', #The name of your choice for this table associated to antiparkinson drugs
+                                       overwrite = TRUE
+) 
+################################################################################
+#                                                                              #
+#                              Setting up for Subtypes                         #
+#                                                                              #
+################################################################################
 
+outcome_cohorts_subtypes <- readCohortSet(here(
+  "1_InstantiateCohorts",
+  "OutcomeCohortsSubtypes"
+))
 
-cdm <- CDMConnector::generateCohortSet(cdm, outcome_cohorts,
-                         cohortTableName = outcome_table_name,
+cdm <- generateCohortSet(cdm = cdm, 
+                         cohortSet =  outcome_cohorts_subtypes,
+                         cohortTableName = '...', #The name of your choice for this table associated to parkinsonism subtypes
                          overwrite = TRUE
 )
-
-
-info(logger, "- got outcomes")
