07860faae681af7178fe77e3a9fcf8fb50af2a8e
xihang-chen <xihang.chen@ndorms.ox.ac.uk>
2023-01-18 11:31:59 +0000

    update

--- 1_InstantiateCohorts/InstantiateStudyCohorts.R
+++ 1_InstantiateCohorts/InstantiateStudyCohorts.R
@@ -1,6 +1,3 @@
-library(CDMConnector)
-library(here)
-
 ################################################################################
 #                                                                              #
 #                              Setting up for Drugs                            #
@@ -11,25 +8,10 @@ outcome_cohorts_drugs <- CDMConnector::readCohortSet(here(
   "1_InstantiateCohorts",
   "OutcomeCohortsDrugs"
 ))
-
-databaseName <- "..."
-server     <- Sys.getenv("DB_SERVER_name_database")
-server_dbi <- Sys.getenv("DB_SERVER_DBI_name_database")
-user       <- Sys.getenv("DB_USER")
-password   <- Sys.getenv("DB_PASSWORD")
-port       <- Sys.getenv("DB_PORT")
-host       <- Sys.getenv("DB_HOST")
-vocabularyDatabaseSchema <- "..."
-dbmsName <- "..."
-con <- DBI::dbConnect(RPostgres::Postgres(), dbname = server_dbi, port = port, host = host, user = user,
-                      password = password)
-cdm <- CDMConnector::cdm_from_con(con = con,
-                                  cdm_schema = '...',
-                                  write_schema = '...')
                                   
 cdm <- CDMConnector::generateCohortSet(cdm = cdm,
                                        cohortSet = outcome_cohorts_drugs,
-                                       cohortTableName = '...', #The name of your choice for this table associated to antiparkinson drugs
+                                       cohortTableName = subtype_table_name, 
                                        overwrite = TRUE
 ) 
 ################################################################################
@@ -45,6 +27,6 @@ outcome_cohorts_subtypes <- readCohortSet(here(
 
 cdm <- generateCohortSet(cdm = cdm, 
                          cohortSet =  outcome_cohorts_subtypes,
-                         cohortTableName = '...', #The name of your choice for this table associated to parkinsonism subtypes
+                         cohortTableName = subtype_table_name, 
                          overwrite = TRUE
 )
